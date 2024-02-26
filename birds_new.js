1b0f4cfd5b45606c24f4f44cebbb55518399d4ec
Bodarnn <103896552+Bodarnn@users.noreply.github.com>
2024-02-26 15:44:09 -0500

    Update birds_new.js

--- birds_new.js
+++ birds_new.js
@@ -1,17 +1,3 @@
-// https://github.com/zloirock/core-js/blob/master/deno/corejs/index.js
-// file:///C:/Users/Brand/Desktop/test/index.html
-
-// arr_1 = [1, 2, 3, 4];
-// arr_2 = [5, 6, 7, 8];
-// obj_1 = {"a": 1, "b": 2, "c": 3, "d": 4};
-// obj_2 = {"e": 5, "f": 6, "g": 7, "h": 8};
-// bd = new Birds.Array();
-
-// Note to self:
-// Copy Array: copy = array.slice();
-// Iterate Object: for...in
-// Iterate Array: for...of
-
 const Birds = { // Birds namespace
 	Array: function (values, keys) { // Array constructor
 		if (values === undefined || values === null) { // values is undefined...
@@ -231,12 +217,3 @@ Birds.Array.prototype[Symbol.iterator] = function () {
 Birds.Array.prototype.myMethod = function () {
 	// Your method code here
 };
-
-// https://minify-js.com/
-// https://unminify.com/
-// Benchmark for getting keys and values
-function benchmarkObject() { const e = {}; for (let o = 0; o < 1e4; o++)e[`k${o}`] = `v${o}`; const o = 1e3; let n, r; console.log("Get Keys"), n = performance.now(); for (let n = 0; n < o; n++) { const o = Object.keys(e) } r = performance.now(), console.log(`Object.keys(): ${(r - n) / o} ms`), n = performance.now(); for (let n = 0; n < o; n++) { let o = []; for (const n in e) e.hasOwnProperty(n) && o.push(n) } r = performance.now(), console.log(`for...in: ${(r - n) / o} ms`), console.log("Get Values"), n = performance.now(); for (let n = 0; n < o; n++) { let o = []; for (const n in e) e.hasOwnProperty(n) && o.push(e[n]) } r = performance.now(), console.log(`for...in: ${(r - n) / o} ms`), n = performance.now(); for (let n = 0; n < o; n++) { const o = Object.values(e) } r = performance.now(), console.log(`Object.values(): ${(r - n) / o} ms`), console.log("Get Entries"), n = performance.now(); for (let n = 0; n < o; n++) { const o = Object.keys(e); let n = []; for (let r = 0; r < o.length; r++)n.push(e[o[r]]) } r = performance.now(), console.log(`Object.keys() & for: ${(r - n) / o} ms`), n = performance.now(); for (let n = 0; n < o; n++) { let o = [], n = []; for (const r in e) e.hasOwnProperty(r) && (o.push(r), n.push(e[r])) } r = performance.now(), console.log(`for...in: ${(r - n) / o} ms`), n = performance.now(); for (let n = 0; n < o; n++) { let o = [], n = []; for (const [r, s] of Object.entries(e)) o.push(r), n.push(s) } r = performance.now(), console.log(`Object.entries(): ${(r - n) / o} ms`), n = performance.now(); for (let n = 0; n < o; n++) { const o = Object.keys(e), n = Object.values(e) } r = performance.now(), console.log(`Object.keys() & Object.values(): ${(r - n) / o} ms`) }
-// Benchmark for copying Arrays
-function benchmarkArray() { const o = Array.from({ length: 1e4 }, ((o, e) => `index${e + 1}`)), e = 1e3; let r, n; console.log("Copy Array"), r = performance.now(); for (let r = 0; r < e; r++) { const e = o.slice() } n = performance.now(), console.log(`Arr.slice(): ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = o.concat() } n = performance.now(), console.log(`Array.concat(): ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = Object.values(o) } n = performance.now(), console.log(`Object.values(): ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = []; for (let r = 0; r < o.length; r++)e.push(o[r]) } n = performance.now(), console.log(`for: ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = o.map((o => o)) } n = performance.now(), console.log(`Array.map(): ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = []; o.forEach((o => e.push(o))) } n = performance.now(), console.log(`Array.forEach(): ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = Array.from(o) } n = performance.now(), console.log(`Array.from(): ${(n - r) / e} ms`), r = performance.now(); for (let r = 0; r < e; r++) { const e = [...o] } n = performance.now(), console.log(`...: ${(n - r) / e} ms`) }
-// Benchmark for loops
-function benchmarkLoop() { const o = Array.from({ length: 1e4 }, ((o, e) => `index${e + 1}`)), e = 1e3; let n, r; n = performance.now(); for (let n = 0; n < e; n++) { let e = []; for (const n of o) e.push(n + 1) } r = performance.now(), console.log(`for...of: ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = []; for (let n = 0; n < o.length; n++)e.push(o[n] + 1) } r = performance.now(), console.log(`for loop (push): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = []; for (let n = 0; n < o.length; n++)e[n] = o[n] + 1 } r = performance.now(), console.log(`for loop (index): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = 0, n = []; for (; e < o.length;)n.push(o[e] + 1), e++ } r = performance.now(), console.log(`while (push): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = 0, n = []; for (; e < o.length;)n[e] = o[e] + 1, e++ } r = performance.now(), console.log(`while (index): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = 0, n = []; do { n.push(o[e] + 1), e++ } while (e < o.length) } r = performance.now(), console.log(`do-while (push): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = 0, n = []; do { n[e] = o[e] + 1, e++ } while (e < o.length) } r = performance.now(), console.log(`do-while (index): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = []; o.forEach((o => { e.push(o + 1) })) } r = performance.now(), console.log(`forEach (push): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = []; o.forEach(((o, n) => { e[n] = o + 1 })) } r = performance.now(), console.log(`forEach (index): ${(r - n) / e} ms`), n = performance.now(); for (let n = 0; n < e; n++) { let e = o.map((o => o + 1)) } r = performance.now(), console.log(`map: ${(r - n) / e} ms`) }
